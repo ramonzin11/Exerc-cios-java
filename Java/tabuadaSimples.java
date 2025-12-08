3ca7087a521c45b2279beccfb5285bb78e84c45f
Ramon Neves <rmfaria12@hotmail.com>
2025-12-07 21:17:02 -0300

    Update tabuadaSimples.java

--- Java/tabuadaSimples.java
+++ Java/tabuadaSimples.java
@@ -6,17 +6,21 @@ public class tabuadaSimples {
 
         System.out.println("=*=*=*=*=* TABUADA SIMPLES =*=*=*=*=*=*");
         
+        //? Nessa primeira etapa o usuario vai digite o número da tabuada que ele deseja ver
         System.out.print("Qual taboada você deseja saber : ");
         int numerotabuada = scanner.nextInt();
 
+        //? vamos desclarar a variavel que vai ser multiplicada pelo número que o usuario definial 
         int multiplicador = 1;
 
+        //? Aqui vai acomtecer a multiplicação 
         while (multiplicador <= 10) {
             int equacao = multiplicador * numerotabuada;
             System.out.println(multiplicador + " X " + numerotabuada + " = " + equacao);
             multiplicador++;
         }
         
+        scanner.close();
 
     }
 }
