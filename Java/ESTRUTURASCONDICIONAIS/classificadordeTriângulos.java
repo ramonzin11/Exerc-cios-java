e3128a19bb7519c69267e9b12e2984f853f1031f
Ramon Neves <rmfaria12@hotmail.com>
2026-01-14 12:04:56 -0300

    Update classificadordeTriângulos.java

--- Java/ESTRUTURASCONDICIONAIS/classificadordeTriângulos.java
+++ Java/ESTRUTURASCONDICIONAIS/classificadordeTriângulos.java
@@ -1,4 +1,5 @@
-    import java.util.Scanner;
+
+import java.util.Scanner;
 public class classificadordeTriângulos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
