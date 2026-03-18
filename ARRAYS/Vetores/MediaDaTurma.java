51480d70a732f31e6c33dc74f5335a35306372b8
Ramon Neves <rmfaria12@hotmail.com>
2026-03-18 00:13:58 -0300

    Média da Turma
    Vetor de 8 notas com Scanner, cálculo de média, contagem de alunos acima da média e busca da maior nota

---
+++ ARRAYS/Vetores/MediaDaTurma.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class MediaDaTurma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] notas = new double[8];
+        double mediaDosAlunos; 
+        double totalDasNotas = 0;
+        int acimaDamedia = 0;
+
+        for(int i = 0; i < notas.length; i++){
+            System.out.println("Digita a nota do " + (i + 1) + "° aluno: ");
+            notas[i] = scanner.nextDouble();
+            totalDasNotas += notas[i];
+        }
+
+        mediaDosAlunos = totalDasNotas / notas.length;
+        double maior = notas[0];
+        
+        for(int i = 0; i < notas.length; i++){
+            if(maior < notas[i]){
+                maior = notas[i];
+            }
+
+            if(notas[i] > mediaDosAlunos){
+                acimaDamedia++;
+            }
+        }
+
+        System.out.println("Alunos acima da média: " + acimaDamedia);
+        System.out.println("A média dos " + notas.length + " alunos: " + mediaDosAlunos);
+        System.out.println("A maior nota: " + maior);
+    }
+}
