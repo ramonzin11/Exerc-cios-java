9ecd11ce883c1d4c74476056a42737f542ab0b20
Ramon Neves <rmfaria12@hotmail.com>
2026-03-27 12:03:35 -0300

    feat: ClassificadorDeTemperatura
    Classificação de temperatura em 5 faixas usando if/else if

---
+++ Estruturas-Condicionais/IF/ClassificadorDeTemperatura.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ClassificadorDeTemperatura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Classificador De Temperatura");
+
+        System.out.println("Qual é a temperatura atual: ");
+        double tempAtual = scanner.nextDouble();
+
+        if(tempAtual < 10){
+            System.out.println("Temperatura muito frio: " + tempAtual + "°");
+        }else if(tempAtual >= 11 && tempAtual <= 17){
+            System.out.println("Temperatura frio: " + tempAtual + "°");
+        }else if(tempAtual >= 18 && tempAtual <= 25){
+            System.out.println("Temperatura Agradável: " + tempAtual + "°");
+        }else if(tempAtual >= 26 && tempAtual <= 35){
+            System.out.println("Temperatura quente: " + tempAtual + "°");
+        }else{
+            System.out.println("Temperatura muito quente: " + tempAtual + "°");
+        }
+
+        scanner.close();
+    }
+}
