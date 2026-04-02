ee77cfcdd47683f831faa9b7f64f2d108f0bef93
Ramon Neves <rmfaria12@hotmail.com>
2026-04-02 14:27:04 -0300

    Dias da Semana
    Dias da Semana: Converte um input numérico (1-7) no nome do dia da semana correspondente.

--- Estruturas-Condicionais/SWITCH/DiasDaSemana.java
+++ Estruturas-Condicionais/SWITCH/DiasDaSemana.java
@@ -1,5 +1,58 @@
-public class Exercicio04 {
+import java.util.Scanner;
+
+public class DiasDaSemana {
     public static void main(String[] args) {
-        // TODO: implementar
+        Scanner scanner = new Scanner(System.in);
+
+        int interacaoDoUsuario;
+        String diaDaSemana;
+        boolean sairDoLoop = false;
+
+        while (!sairDoLoop) { // Adicionado o "!" para o loop funcionar
+
+            System.out.println("\nDigite um número de 1 a 7 (ou 0 para sair): ");
+            interacaoDoUsuario = scanner.nextInt();
+
+            switch (interacaoDoUsuario) {
+                case 1:
+                    diaDaSemana = "Domingo";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 2:
+                    diaDaSemana = "Segunda-Feira";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 3:
+                    diaDaSemana = "Terça-Feira";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 4:
+                    diaDaSemana = "Quarta-Feira";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 5:
+                    diaDaSemana = "Quinta-Feira";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 6:
+                    diaDaSemana = "Sexta-Feira";
+                    System.out.println(diaDaSemana);
+                    break; // Faltava este break
+                case 7:
+                    diaDaSemana = "Sábado";
+                    System.out.println(diaDaSemana);
+                    break;
+                case 0: // Adicionada opção para sair do loop
+                    sairDoLoop = true;
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        }
+
+        scanner.close();
     }
+    
 }
