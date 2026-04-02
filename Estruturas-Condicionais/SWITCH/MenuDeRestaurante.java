a990b2a97b361a57f17e4e222e18fc1cfbb4fc37
Ramon Neves <rmfaria12@hotmail.com>
2026-04-02 14:27:25 -0300

    Menu de Restaurante
    Simula a exibição de um cardápio com nomes e valores de pratos específicos.

--- Estruturas-Condicionais/SWITCH/MenuDeRestaurante.java
+++ Estruturas-Condicionais/SWITCH/MenuDeRestaurante.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class MenuDeRestaurante {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int interacaoDoUsuario;
         boolean sairDoLoop = false;
 
-        String nomeDoPrato01 = "MACARRÇÃO COM MOLHO BRANCO";
+        String nomeDoPrato01 = "MACARRÃO COM MOLHO BRANCO";
         double valorDoPrato01 = 50.00;
 
         String nomeDoPrato02 = "LASANHA DE FRANGO";
@@ -17,42 +18,44 @@ public class MenuDeRestaurante {
         String nomeDoPrato04 = "FRANGO GRELHADO";
         double valorDoPrato04 = 60.00;
 
-        String nomeDoPrato05 = "CAMARÃO COM MOLH0";
+        String nomeDoPrato05 = "CAMARÃO COM MOLHO";
         double valorDoPrato05 = 70.00;
 
         while (!sairDoLoop) {
-            System.out.println("1: PRATO 1");
+            System.out.println("\n1: PRATO 1");
             System.out.println("2: PRATO 2");
             System.out.println("3: PRATO 3");
             System.out.println("4: PRATO 4");
             System.out.println("5: PRATO 5");
-            System.out.println("0 Sair do menu");
-            System.out.print("\nEscolha qual operação deseja realizar!");
+            System.out.println("0: Sair do menu");
+            System.out.print("Escolha qual operação deseja realizar: ");
             interacaoDoUsuario = scanner.nextInt();
 
             switch (interacaoDoUsuario) {
                 case 1:
-                    System.out.println("NOME DO PRATO: " + nomeDoPrato01 + " | VALOR DO PRATO: " + valorDoPrato01);
+                    System.out.println("NOME DO PRATO: " + nomeDoPrato01 + " | VALOR DO PRATO: R$" + valorDoPrato01);
                     break;
                 case 2:
-                    System.out.println("NOME DO PRATO: " + nomeDoPrato02 + " | VALOR DO PRATO: " + valorDoPrato02);
+                    System.out.println("NOME DO PRATO: " + nomeDoPrato02 + " | VALOR DO PRATO: R$" + valorDoPrato02);
                     break;
                 case 3:
-                    System.out.println("NOME DO PRATO: " + nomeDoPrato03 + " | VALOR DO PRATO: " + valorDoPrato03); 
+                    System.out.println("NOME DO PRATO: " + nomeDoPrato03 + " | VALOR DO PRATO: R$" + valorDoPrato03);
                     break;
                 case 4:
-                    System.out.println("NOME DO PRATO: " + nomeDoPrato04 + " | VALOR DO PRATO: " + valorDoPrato04); 
+                    System.out.println("NOME DO PRATO: " + nomeDoPrato04 + " | VALOR DO PRATO: R$" + valorDoPrato04);
                     break;
                 case 5:
-                    System.out.println("NOME DO PRATO: " + nomeDoPrato05 + " | VALOR DO PRATO: " + valorDoPrato05);           
+                    System.out.println("NOME DO PRATO: " + nomeDoPrato05 + " | VALOR DO PRATO: R$" + valorDoPrato05);
+                    break; // Faltava este break
+                case 0: // Faltava o case para sair do loop
+                    sairDoLoop = true;
+                    break;
                 default:
-                    System.out.println("Opção invalida!!!");
+                    System.out.println("Opção inválida!!!");
                     break;
             }
         }
 
         scanner.close();
-
-
     }
 }
