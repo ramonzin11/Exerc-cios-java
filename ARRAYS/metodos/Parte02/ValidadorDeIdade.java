6c855a4b465b4426b585a16fece94c7e7c2f2da1
Ramon Neves <rmfaria12@hotmail.com>
2026-07-21 22:06:40 -0300

    feat: validador de idade com metodos

---
+++ ARRAYS/metodos/Parte02/ValidadorDeIdade.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ValidadorDeIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int idade; 
+
+        System.out.println("Informe sua idade: ");
+        idade = scanner.nextInt();
+
+        System.out.println(verificarIdade(idade));
+
+        scanner.close();
+
+    }
+
+    public static String verificarIdade(int idade){
+        if(idade >= 0 && idade <= 12){
+          return "Criança";
+        }else if(idade >= 13 && idade <= 17){
+            return "Adolescente";
+        }else if(idade >= 18 && idade <=59){
+            return "Adulto";
+        }else if(idade > 60){
+           return "Idoso";
+        }else{
+            return "Idade inválida";
+        }
+    }
+}
